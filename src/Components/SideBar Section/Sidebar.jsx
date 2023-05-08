d96e709700c506040ec816516feb1629dd994e9b
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-05-08 20:47:35 +0700

    19.06

--- src/Components/SideBar Section/Sidebar.jsx
+++ src/Components/SideBar Section/Sidebar.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './sidebar.css'
 
+// import image
+import logo from '../../Assets/bima.png'
+
+// import icon
+import { IoMdSpeedometer } from 'react-icons/io'
+
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className='sideBar grid'>
+
+            <div className="logoDiv flex">
+                {/* <img src={logo} alt="Image Name" /> */}
+                <h2>bima.Krisnawan</h2>
+            </div>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">
+                    Quick Menu
+                </h3>
+                <ul className="menuLists grid">
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smallText">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smallText">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smallText">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smallText">
+                                Dashboard
+                            </span>
+                        </a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+
+        </div>
+
     )
 }
 
