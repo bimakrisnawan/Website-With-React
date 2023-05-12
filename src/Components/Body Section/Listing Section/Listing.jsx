a8905860eca54daff13eef85f6ba9d48f0d434f3
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-05-13 06:48:56 +0700

    01.02.37

--- src/Components/Body Section/Listing Section/Listing.jsx
+++ src/Components/Body Section/Listing Section/Listing.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './listing.css'
 
+// image
+import img from '../../../Assets/bima.png'
+
+
+// icon
+import { BsArrowRightShort } from 'react-icons/bs';
+import { AiFillHeart } from 'react-icons/ai';
+
 const Listing = () => {
     return (
-        <div>Listing</div>
+        <div className='listingSection'>
+
+            <div className="heading flex">
+                <h1>My Listings</h1>
+                <button className='btn flex'>
+                    See All
+                    <BsArrowRightShort className="icon" />
+                </button>
+            </div>
+
+            <div className="secContainer flex">
+                <div className="singleItem">
+                    <AiFillHeart className="icon" />
+                    <img src={img} alt="Image Name" />
+                    <h3>Anual Vince</h3>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Body Section/Top Section/Top.jsx
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -5,9 +5,11 @@ import './top.css'
 import { BiSearchAlt } from 'react-icons/bi';
 import { TbMessageCircle } from 'react-icons/tb';
 import { MdNotificationsNone } from 'react-icons/md';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 // import image
 import img from '../../../Assets/profil.jpg'
+import img2 from '../../../Assets/bima.png'
 import video from '../../../Assets/video_dsh/video.mp4'
 
 const Top = () => {
@@ -53,9 +55,42 @@ const Top = () => {
 
                 <div className="leftCard flex">
                     <div className="main flex">
+
                         <div className="textDiv">
                             <h1>My Stat</h1>
+
+                            <div className="flex">
+                                <span>
+                                    Today <small>4 Orders</small>
+                                </span>
+
+                                <span>
+                                    This Month <small>23 Orders</small>
+                                </span>
+                            </div>
+
+                            <span className="flex link">
+                                Go to my orders
+                                <BsArrowRightShort className="icon" />
+                            </span>
+
                         </div>
+
+                        <div className="imgDiv">
+                            <img src={img2} alt="Image Name" />
+                        </div>
+
+                        {/* <div className='sideBarCard'>
+                            <BsQuestionCircle className="icon" />
+                            <div className="cardContent">
+                                <div className="circle1"></div>
+                                <div className="circle2"></div>
+
+                                <h3>Help Center</h3>
+                                <p>Having question? Contact us to know your problem!</p>
+                                <button className='btn'>Go to help center</button>
+                            </div>
+                        </div> */}
 
                     </div>
                 </div>
