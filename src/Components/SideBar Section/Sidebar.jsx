43c349dfbf9235ca13f7556d3cb5584423e7f418
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-05-12 14:03:57 +0700

    33.07

--- src/Components/SideBar Section/Sidebar.jsx
+++ src/Components/SideBar Section/Sidebar.jsx
@@ -6,14 +6,23 @@ import logo from '../../Assets/bima.png'
 
 // import icon
 import { IoMdSpeedometer } from 'react-icons/io'
+import { MdDeliveryDining } from 'react-icons/md'
+import { MdOutlineExplore } from 'react-icons/md'
+import { BsTrophy } from 'react-icons/bs'
+import { AiOutlinePieChart } from 'react-icons/ai'
+import { BiTrendingUp } from 'react-icons/bi'
+import { MdOutlinePermContactCalendar } from 'react-icons/md'
+import { BsCreditCard2Front } from 'react-icons/bs'
+import { BsQuestionCircle } from 'react-icons/bs'
 
 const Sidebar = () => {
     return (
+
         <div className='sideBar grid'>
 
             <div className="logoDiv flex">
-                {/* <img src={logo} alt="Image Name" /> */}
-                <h2>bima.Krisnawan</h2>
+                <img src={logo} alt="Image Name" />
+                <h2>Bima Krisnawan.</h2>
             </div>
 
             <div className="menuDiv">
@@ -33,27 +42,27 @@ const Sidebar = () => {
 
                     <li className="listItem">
                         <a href="#" className='menuLink flex'>
-                            <IoMdSpeedometer className="icon" />
+                            <MdDeliveryDining className="icon" />
                             <span className="smallText">
-                                Dashboard
+                                My Orders
                             </span>
                         </a>
                     </li>
 
                     <li className="listItem">
                         <a href="#" className='menuLink flex'>
-                            <IoMdSpeedometer className="icon" />
+                            <MdOutlineExplore className="icon" />
                             <span className="smallText">
-                                Dashboard
+                                Explore
                             </span>
                         </a>
                     </li>
 
                     <li className="listItem">
                         <a href="#" className='menuLink flex'>
-                            <IoMdSpeedometer className="icon" />
+                            <BsTrophy className="icon" />
                             <span className="smallText">
-                                Dashboard
+                                Product
                             </span>
                         </a>
                     </li>
@@ -61,6 +70,65 @@ const Sidebar = () => {
 
 
                 </ul>
+            </div>
+
+            <div className="settingsDiv">
+                <h3 className="divTitle">
+                    Settings
+                </h3>
+                <ul className="menuLists grid">
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <AiOutlinePieChart className="icon" />
+                            <span className="smallText">
+                                Chart
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <BiTrendingUp className="icon" />
+                            <span className="smallText">
+                                Trends
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <MdOutlinePermContactCalendar className="icon" />
+                            <span className="smallText">
+                                Calendar
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="listItem">
+                        <a href="#" className='menuLink flex'>
+                            <BsCreditCard2Front className="icon" />
+                            <span className="smallText">
+                                Card
+                            </span>
+                        </a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+
+            <div className='sideBarCard'>
+                <BsQuestionCircle className="icon" />
+                <div className="cardContent">
+                    <div className="circle1"></div>
+                    <div className="circle1"></div>
+
+                    <h3>Help Center</h3>
+                    <p>Having question? Contact us to know your problem!</p>
+                    <button className='btn'>Go to help center</button>
+                </div>
             </div>
 
         </div>
