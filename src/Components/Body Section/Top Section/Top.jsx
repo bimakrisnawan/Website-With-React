b215151c8d91bcfa0767039c656f2789df1f1dfe
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-05-12 22:56:57 +0700

    51.20

--- src/Components/Body Section/Top Section/Top.jsx
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -8,6 +8,7 @@ import { MdNotificationsNone } from 'react-icons/md';
 
 // import image
 import img from '../../../Assets/profil.jpg'
+import video from '../../../Assets/video_dsh/video.mp4'
 
 const Top = () => {
     return (
@@ -33,15 +34,32 @@ const Top = () => {
             </div>
 
             <div className="cardSection flex">
+
                 <div className="rightCard flex">
+
                     <h1>Lorem ipsum dolor sit amet.</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, molestias!</p>
 
                     <div className="buttons flex">
-                        <button className='btn'> Explore More</button>
-                        <button className='btn transparent'> Top Sellers</button>
+                        <button className='btn'>Explore More</button>
+                        <button className='btn transparent'>Top Sellers</button>
+                    </div>
+
+                    <div className="videoDiv">
+                        <video src={video} autoPlay loop muted></video>
+                    </div>
+
+                </div>
+
+                <div className="leftCard flex">
+                    <div className="main flex">
+                        <div className="textDiv">
+                            <h1>My Stat</h1>
+                        </div>
+
                     </div>
                 </div>
+
             </div>
         </div>
     )
