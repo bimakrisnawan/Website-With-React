bba8244b2c28c5e78ee85de6fd3393123ae5b40e
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-05-14 20:40:19 +0700

    1.21.24
    not responsive yet

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
 import './activity.css'
 
+// import icon
+import { BsArrowRightShort } from 'react-icons/bs'
+
+// import image
+import img from '../../../Assets/profil.jpg'
+
+
 const Activity = () => {
     return (
-        <div>Activity</div>
+        <div className='activitySection'>
+            <div className="heading flex">
+                <h2>Recent Activity</h2>
+                <button className='btn flex'>
+                    See All
+                    <BsArrowRightShort className='icon' />
+                </button>
+            </div>
+
+            <div className="secContainer grid">
+
+                <div className="singleCustomer flex">
+                    <img src={img} alt="Customer Image" />
+                    <div className="customerDetails">
+                        <span className="name">Krisnawan</span>
+                        <small>
+                            Ordered something from you!
+                        </small>
+                    </div>
+                    <div className="duration">
+                        12 min ago
+                    </div>
+                </div>
+
+                <div className="singleCustomer flex">
+                    <img src={img} alt="Customer Image" />
+                    <div className="customerDetails">
+                        <span className="name">Krisnawan</span>
+                        <small>
+                            Ordered something from you!
+                        </small>
+                    </div>
+                    <div className="duration">
+                        12 min ago
+                    </div>
+                </div>
+
+                <div className="singleCustomer flex">
+                    <img src={img} alt="Customer Image" />
+                    <div className="customerDetails">
+                        <span className="name">Krisnawan</span>
+                        <small>
+                            Ordered something from you!
+                        </small>
+                    </div>
+                    <div className="duration">
+                        12 min ago
+                    </div>
+                </div>
+
+                <div className="singleCustomer flex">
+                    <img src={img} alt="Customer Image" />
+                    <div className="customerDetails">
+                        <span className="name">Krisnawan</span>
+                        <small>
+                            Ordered something from you!
+                        </small>
+                    </div>
+                    <div className="duration">
+                        12 min ago
+                    </div>
+                </div>
+
+                <div className="singleCustomer flex">
+                    <img src={img} alt="Customer Image" />
+                    <div className="customerDetails">
+                        <span className="name">Krisnawan</span>
+                        <small>
+                            Ordered something from you!
+                        </small>
+                    </div>
+                    <div className="duration">
+                        12 min ago
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/Components/Body Section/Listing Section/Listing.jsx
+++ src/Components/Body Section/Listing Section/Listing.jsx
@@ -3,6 +3,7 @@ import './listing.css'
 
 // image
 import img from '../../../Assets/bima.png'
+import img2 from '../../../Assets/profil.jpg'
 
 
 // icon
@@ -22,12 +23,98 @@ const Listing = () => {
             </div>
 
             <div className="secContainer flex">
+
+
                 <div className="singleItem">
                     <AiFillHeart className="icon" />
                     <img src={img} alt="Image Name" />
-                    <h3>Anual Vince</h3>
+                    <h3>Anual Vince 1</h3>
                 </div>
+
+                <div className="singleItem">
+                    <AiFillHeart className="icon" />
+                    <img src={img} alt="Image Name" />
+                    <h3>Anual Vince 2</h3>
+                </div>
+
+                <div className="singleItem">
+                    <AiFillHeart className="icon" />
+                    <img src={img} alt="Image Name" />
+                    <h3>Anual Vince 3</h3>
+                </div>
+
+                <div className="singleItem">
+                    <AiFillHeart className="icon" />
+                    <img src={img} alt="Image Name" />
+                    <h3>Anual Vince 4</h3>
+                </div>
+
+
             </div>
+
+            <div className="sellers flex">
+
+                <div className="topSellers">
+                    <div className="heading flex">
+                        <h3>Top Sellers</h3>
+                        <button className='btn flex'>
+                            See All
+                            <BsArrowRightShort className="icon" />
+                        </button>
+                    </div>
+
+                    <div className="card flex">
+                        <div className="users">
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                        </div>
+                        <div className="cardText">
+                            <span>
+                                123 Stalker Checked <br />
+                                <small>
+                                    32 Sellers <span className="date">7 Days</span>
+                                </small>
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="featuredSeller">
+                    <div className="heading flex">
+                        <h3>Featured Sellers</h3>
+                        <button className='btn flex'>
+                            See All
+                            <BsArrowRightShort className="icon" />
+                        </button>
+                    </div>
+
+                    <div className="card flex">
+                        <div className="users">
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                            <img src={img2} alt="User Image" />
+                        </div>
+                        <div className="cardText">
+                            <span>
+                                144 Stalker Checked <br />
+                                <small>
+                                    234 Sellers <span className="date">7 Days</span>
+                                </small>
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+
         </div>
     )
 }
